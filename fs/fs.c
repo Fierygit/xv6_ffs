98e56dde5dc8908b6e1a1f1055eb94ba6f18d1bb
fireflychen <fireflychen@tencent.com>
2020-09-08 22:25:15 +0800

    update

--- fs/fs.c
+++ fs/fs.c
@@ -51,32 +51,32 @@ static void bzero(int dev, int bno) {
 // Blocks.
 
 // Allocate a zeroed disk block.
-static uint balloc(uint dev) {
-  int b, bi, m;
-  struct buf *bp;
-  struct buf *sbp;
+// static uint balloc(uint dev) {
+//   int b, bi, m;
+//   struct buf *bp;
+//   struct buf *sbp;
 
-  bp = 0;
-  for (b = 0; b < sb.size; b += BPB) {
-    bp = bread(dev, BBLOCK(b, sb));
-    for (bi = 0; bi < BPB && b + bi < sb.size; bi++) {
-      m = 1 << (bi % 8); // 取出标志位
-      if ((bp->data[bi / 8] & m) == 0) {  // Is block free?
-        bp->data[bi / 8] |= m;            // Mark block in use.
-        log_write(bp);
-        brelse(bp);
-        bzero(dev, b + bi);
-        sbp = bread(dev, 1);
-        // 这里记录一下
-        sbp->data[sizeof(bp->data) -  (b+bi)/ GSIZE - 1]++;
-        brelse(sbp);
-        return b + bi;
-      }
-    }
-    brelse(bp);
-  }
-  panic("balloc: out of blocks");
-}
+//   bp = 0;
+//   for (b = 0; b < sb.size; b += BPB) {
+//     bp = bread(dev, BBLOCK(b, sb));
+//     for (bi = 0; bi < BPB && b + bi < sb.size; bi++) {
+//       m = 1 << (bi % 8); // 取出标志位
+//       if ((bp->data[bi / 8] & m) == 0) {  // Is block free?
+//         bp->data[bi / 8] |= m;            // Mark block in use.
+//         log_write(bp);
+//         brelse(bp);
+//         bzero(dev, b + bi);
+//         sbp = bread(dev, 1);
+//         // 这里记录一下
+//         sbp->data[sizeof(bp->data) -  (b+bi)/ GSIZE - 1]++;
+//         brelse(sbp);
+//         return b + bi;
+//       }
+//     }
+//     brelse(bp);
+//   }
+//   panic("balloc: out of blocks");
+// }
 
 // **************************************************************
 // simple ffs
